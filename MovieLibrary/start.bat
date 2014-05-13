503667a4d41a160523fc061e8318be23038eed4c
JJ-Author <jj-author@fake22.de>
2014-05-13 22:24:32 +0200

    fixed path issue for startup script in windows

--- MovieLibrary/start.bat
+++ MovieLibrary/start.bat
@@ -1,1 +1,3 @@
-java -Djava.library.path=%~dp0lib\ -cp "%~dp0bin;%~dp0lib\*" jffsss.launch.MovieLibraryLauncher  --WorkingDirectory=%~dp0
+cd /d %~dp0
+set var=%cd%
+java -Djava.library.path=lib\ -cp "bin;lib\*" jffsss.launch.MovieLibraryLauncher --WorkingDirectory="%var%" --FileExtensions="mkv,iso,avi,mp4,mpg,mpeg,mov,divx,xvid,iso,ts"
--- MovieLibrary/start.sh
+++ MovieLibrary/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+P='"'
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIRF=$P$DIR$P
 LIB="/lib"
 BIN="/bin:"
 LIBF=$DIR$LIB
@@ -7,8 +9,11 @@ BINF=$DIR$BIN
 STAR="/*"
 LIBS=$LIBF$STAR
 CP=$BINF$LIBS
+echo $DIRF
 echo $LIBF
 echo $BINF
 echo $CP
 #java -Djava.library.path=./lib -cp ./bin:./lib/* jffsss.launch.MovieLibraryLauncher --WorkingDirectory=$DIR
-java -Djava.library.path=$LIBF -cp /home/hadoop/Desktop/git/MovieLibrary/MovieLibrary/bin:/home/hadoop/Desktop/git/MovieLibrary/MovieLibrary/lib/* jffsss.launch.MovieLibraryLauncher --WorkingDirectory=$DIR
+cd $DIR
+java -Djava.library.path=./lib -cp ./bin:./lib/* jffsss.launch.MovieLibraryLauncher --WorkingDirectory=$DIRF
+#/usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java -Djava.library.path=$LIBF -cp $CP jffsss.launch.MovieLibraryLauncher --WorkingDirectory=$DIR --FileExtensions="mkv,iso,avi,mp4,mpg,mpeg,mov,divx,xvid,iso,ts"
