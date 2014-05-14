efebeb498f9ffdb460701f1f1235d40e3336499a
JJ-Author <jj-author@fake22.de>
2014-05-14 11:55:14 +0200

    fixed path with space issue for unix startup script

--- MovieLibrary/start.sh
+++ MovieLibrary/start.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 P='"'
+O='"'
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-DIRF=$P$DIR$P
-LIB="/lib"
-BIN="/bin:"
-LIBF=$DIR$LIB
-BINF=$DIR$BIN
-STAR="/*"
-LIBS=$LIBF$STAR
-CP=$BINF$LIBS
-echo $DIRF
-echo $LIBF
-echo $BINF
-echo $CP
-#java -Djava.library.path=./lib -cp ./bin:./lib/* jffsss.launch.MovieLibraryLauncher --WorkingDirectory=$DIR
-cd $DIR
-java -Djava.library.path=./lib -cp ./bin:./lib/* jffsss.launch.MovieLibraryLauncher --WorkingDirectory=$DIRF
-#/usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java -Djava.library.path=$LIBF -cp $CP jffsss.launch.MovieLibraryLauncher --WorkingDirectory=$DIR --FileExtensions="mkv,iso,avi,mp4,mpg,mpeg,mov,divx,xvid,iso,ts"
+DIRF=$P$DIR$O
+eval cd $DIRF
+eval java -Djava.library.path=./lib -cp ./bin:./lib/* jffsss.launch.MovieLibraryLauncher --WorkingDirectory=$DIRF --FileExtensions="mkv,iso,avi,mp4,mpg,mpeg,mov,divx,xvid,iso,ts"
+#eval /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java -Djava.library.path=./lib -cp ./bin:./lib/* jffsss.launch.MovieLibraryLauncher --WorkingDirectory=$DIRF --FileExtensions="mkv,iso,avi,mp4,mpg,mpeg,mov,divx,xvid,iso,ts"
